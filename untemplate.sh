405a773d8450c26d5e7dcda3b03efee85f176d74
albertwcheng <albertwcheng@gmail.com>
2010-11-06 23:09:45 -0400

    change untemplate.sh

--- untemplate.sh
+++ untemplate.sh
@@ -5,7 +5,7 @@ for i in *.template; do
 	untmpName=${i/.template/}
 	echo -n untemplating $untmpName
 	if [ -e $untmpName ]; then
-		echo " not copied." existed already
+		echo " not copied. existed already. Use untemplate -f to force overwrite"
 	else
 		echo " copied"
 		cp $i $untmpName
