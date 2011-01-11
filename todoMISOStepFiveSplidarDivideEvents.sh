db9ff2f240f3a1b4fa1b888278c21a8b82cc71b1
albertwcheng <albertwcheng@gmail.com>
2011-01-11 00:49:55 -0500

    commit

---
+++ todoMISOStepFiveSplidarDivideEvents.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+:<<'COMMENT'
+
+cheng@tak /lab/solexa_jaenisch4/Albert/mRNASeqLibs/ESC_EpiSC/MISOOnPE_splidarEvents/MISOComparisons/Alb_mEpiSCLib2-Alb_mESCLib2/Alb_mEpiSCLib2_vs_Alb_mESCLib2/bayes-factors$ colStat.py -r1,2 *.tab
+[:::::			R 1			:::::]
+Index			Excel			Field
+-----			-----			-----
+1			A			eventIDString
+2			B			sample1_posterior_mean
+3			C			sample1_ci_low
+4			D			sample1_ci_high
+5			E			sample2_posterior_mean
+6			F			sample2_ci_low
+7			G			sample2_ci_high
+8			H			diff
+9			I			bayes_factor
+[:::::			R 2			:::::]
+Index			Excel			Field
+-----			-----			-----
+1			A			A5SS.Jak3andInsl3.10101
+2			B			0.28
+3			C			0.08
+4			D			0.46
+5			E			0.29
+6			F			0.10
+7			G			0.47
+8			H			-0.01
+9			I			0.50
+
+
+
+COMMENT
+
+if [ $# -lt 1 ]; then
+	echo $0 MISOSettingFile
+	exit
+fi
+
+MISOSettingFile=$1
+
+source ${MISOSettingFile}
+
+thisScriptDir=`pwd`
+
+cd ..
+
+rootDir=`pwd`
+
+
+tophatOutputDir=$rootDir/$bamFileSubRoot
+MISOOutputDir=$rootDir/${MISOOutSubRoot}/MISOOutput
+MISOSummaryDir=$rootDir/${MISOOutSubRoot}/MISOSummary
+MISOComparisonsDir=$rootDir/${MISOOutSubRoot}/MISOComparisons
+
+if [ ! -e $MISOComparisonsDir ]; then
+	echo $MISOComparisonsDir not exists. Abort
+	exit;
+fi
+
+theAwkProgram='function abs(x){if(x<0){return -x;}return x;}{split($eventIDStringCol,a,"."); if((a[1]==eventType && abs($dPsiCol)>=dPsiCutOff && $bayesFactorCol>=bfThreshold )  || FNR==1){print;}}'
+
+for misobfFilename in $MISOComparisonsDir/*/*/bayes-factors/*.miso_bf.splidarEvents.tab; do
+
+bnMISOBFFILENAME=`basename $misobfFilename` #Alb_mEpiSCLib2_vs_Alb_mESCLib2.miso_bf.splidarEvents.tab
+comparisonName=${bnMISOBFFILENAME/.miso_bf.splidarEvents.tab/} #Alb_mEpiSCLib2_vs_Alb_mESCLib2
+parentDir=`absdirname.py $misobfFilename` #/lab/solexa_jaenisch4/Albert/mRNASeqLibs/ESC_EpiSC/MISOOnPE_splidarEvents/MISOComparisons/Alb_mEpiSCLib2-Alb_mESCLib2/Alb_mEpiSCLib2_vs_Alb_mESCLib2/bayes-factors
+	
+	
+	for splidarEventType in ${splidarEventTypes[@]}; do
+		echo making $splidarEventType for comparison ${comparisonName}
+		thisEventTypeFolder=${parentDir}/${splidarEventType} #/lab/solexa_jaenisch4/Albert/mRNASeqLibs/ESC_EpiSC/MISOOnPE_splidarEvents/MISOComparisons/Alb_mEpiSCLib2-Alb_mESCLib2/Alb_mEpiSCLib2_vs_Alb_mESCLib2/bayes-factors/SE/
+		mkdir $thisEventTypeFolder
+		
+		eventIDStringCol=`colSelect.py ${misobfFilename} .eventIDString`
+		dPsiCol=`colSelect.py ${misobfFilename} .diff`
+		bayesFactorCol=`colSelect.py ${misobfFilename} .bayes_factor`
+		
+		awk -v FS="\t" -v OFS="\t" -v eventType=${splidarEventType} -v eventIDStringCol=$eventIDStringCol -v dPsiCol=$dPsiCol -v dPsiCutOff=0.0 -v bayesFactorCol=$bayesFactorCol -v bfThreshold=0.0 'function abs(x){if(x<0){return -x;}return x;}{split($eventIDStringCol,a,"."); if((a[1]==eventType && abs($dPsiCol)>=dPsiCutOff && $bayesFactorCol>=bfThreshold )  || FNR==1){print;}}' ${misobfFilename} > ${thisEventTypeFolder}/${comparisonName}.${splidarEventType}.miso_bf.splidarEvents.tab
+		
+		for bfThreshold in ${bfThresholds[@]}; do
+			awk -v FS="\t" -v OFS="\t" -v eventType=${splidarEventType} -v eventIDStringCol=$eventIDStringCol -v dPsiCol=$dPsiCol -v dPsiCutOff=0.0 -v bayesFactorCol=$bayesFactorCol -v bfThreshold=${bfThreshold} 'function abs(x){if(x<0){return -x;}return x;}{split($eventIDStringCol,a,"."); if((a[1]==eventType && abs($dPsiCol)>=dPsiCutOff && $bayesFactorCol>=bfThreshold )  || FNR==1){print;}}' ${misobfFilename} > ${thisEventTypeFolder}/${comparisonName}.${splidarEventType}.bf${bfThreshold}.miso_bf.splidarEvents.tab
+			
+			for dPsiCutOff in ${dPsiCutOffs[@]}; do
+				awk -v FS="\t" -v OFS="\t" -v eventType=${splidarEventType} -v eventIDStringCol=$eventIDStringCol -v dPsiCol=$dPsiCol -v dPsiCutOff=${dPsiCutOff} -v bayesFactorCol=$bayesFactorCol -v bfThreshold=${bfThreshold} 'function abs(x){if(x<0){return -x;}return x;}{split($eventIDStringCol,a,"."); if((a[1]==eventType && abs($dPsiCol)>=dPsiCutOff && $bayesFactorCol>=bfThreshold )  || FNR==1){print;}}' ${misobfFilename} > ${thisEventTypeFolder}/${comparisonName}.${splidarEventType}.bf${bfThreshold}.dPsi${dPsiCutOff}.miso_bf.splidarEvents.tab			
+			
+			done
+		
+		done
+		
+	done
+
+done
