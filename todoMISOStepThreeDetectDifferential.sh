f71410f896ef5bbd70f0a43d2bae530fdf041241
albertwcheng <albertwcheng@gmail.com>
2011-01-24 18:34:16 -0500

    commit

--- todoMISOStepThreeDetectDifferential.sh
+++ todoMISOStepThreeDetectDifferential.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-if [ $# -lt 1 ]; then
-	echo $0 MISOSettingFile
+if [ $# -ne 2 ]; then
+	echo $0 MISOSettingFile "mode[normal|reduced]"
 	exit
 fi
 
 MISOSettingFile=$1
+mode=$2
 
 source ${MISOSettingFile}
 
@@ -23,14 +24,27 @@ rootDir=`pwd`
 cd $MISOPATH 
 
 tophatOutputDir=$rootDir/$bamFileSubRoot
-MISOOutputDir=$rootDir/${MISOOutSubRoot}/MISOOutput
-MISOSummaryDir=$rootDir/${MISOOutSubRoot}/MISOSummary
-MISOComparisonsDir=$rootDir/${MISOOutSubRoot}/MISOComparisons
+
+if [[ $mode == "normal" ]]; then
+	echo differential using the normal MISO mode
+	MISOOutputDir=$rootDir/${MISOOutSubRoot}/MISOOutput
+	MISOSummaryDir=$rootDir/${MISOOutSubRoot}/MISOSummary
+	MISOComparisonsDir=$rootDir/${MISOOutSubRoot}/MISOComparisons
+elif [[ $mode == "reduced" ]]; then
+	echo differential using the MISO full-transcript reduced to Splidar mode
+	MISOOutputDir=$rootDir/${MISOOutSubRoot}/MISOOutput.reduced
+	MISOSummaryDir=$rootDir/${MISOOutSubRoot}/MISOSummary.reduced
+	MISOComparisonsDir=$rootDir/${MISOOutSubRoot}/MISOComparisons.reduced	
+else
+	echo unknown summary mode $mode
+	bash $0
+	exit
+fi	
 
 mkdir $MISOComparisonsDir
 
 
-# summarize
+# differential, all pairwise, all directions
 
 for sampleDirx in $tophatOutputDir/*; do
 sampleNamex=`basename $sampleDirx`
--- todoMISOStepTwoSummarize.sh
+++ todoMISOStepTwoSummarize.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-if [ $# -lt 1 ]; then
-	echo $0 MISOSettingFile
+if [ $# -ne 2 ]; then
+	echo $0 MISOSettingFile "mode[normal|reduced]"
 	exit
 fi
 
 MISOSettingFile=$1
+mode=$2
 
 source ${MISOSettingFile}
 
@@ -23,8 +24,23 @@ rootDir=`pwd`
 cd $MISOPATH 
 
 tophatOutputDir=$rootDir/$bamFileSubRoot
-MISOOutputDir=$rootDir/${MISOOutSubRoot}/MISOOutput
-MISOSummaryDir=$rootDir/${MISOOutSubRoot}/MISOSummary
+
+if [[ $mode == "normal" ]]; then
+	echo summarizing the normal MISO mode
+	MISOOutputDir=$rootDir/${MISOOutSubRoot}/MISOOutput
+	MISOSummaryDir=$rootDir/${MISOOutSubRoot}/MISOSummary
+elif [[ $mode == "reduced" ]]; then
+	echo summarizing the MISO full-transcript reduced to Splidar mode
+	MISOOutputDir=$rootDir/${MISOOutSubRoot}/MISOOutput.reduced
+	MISOSummaryDir=$rootDir/${MISOOutSubRoot}/MISOSummary.reduced
+else
+	echo unknown summary mode $mode
+	bash $0
+	exit
+fi
+
+echo "using MISOOutput folder $MISOOutputDir"
+echo "output to MISOSummary folder $MISOSummaryDir"
 
 mkdir $MISOSummaryDir
 
