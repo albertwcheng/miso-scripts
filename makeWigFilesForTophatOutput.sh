41754dd8f4b67c1b4e8ea0381dae0b3f3d270315
albertwcheng <albertwcheng@gmail.com>
2011-01-28 12:11:51 -0500

    commit

--- makeWigFilesForTophatOutput.sh
+++ makeWigFilesForTophatOutput.sh
@@ -51,9 +51,9 @@ fi
 #gzip < $sampleName.log10RPKM.wig > $sampleName.log10RPKM.wig.gz
 
 #now let's make a track header
-if [ ! -e accepted_hits.sorted.bam.flagstat ]; then
-	samtools flagstat accepted_hits.sorted.bam > accepted_hits.sorted.bam.flagstat
-fi
+#if [ ! -e accepted_hits.sorted.bam.flagstat ]; then
+samtools flagstat accepted_hits.sorted.bam > accepted_hits.sorted.bam.flagstat
+#fi
 
 numOfReads=`awk -v FS=" " '{if(FNR==4 && $2=="mapped"){printf("%s\n",$1);}}' accepted_hits.sorted.bam.flagstat`
 
@@ -63,8 +63,8 @@ randSuffix=`basename $tmName`
 
 awk -v FS="\t" -v OFS="\t" -v numOfReads=$numOfReads '{if(FNR>1 && $2<$3 ){$4=$4*1000000.0/numOfReads; print;}}' coverage.wig > ${sampleName}.RPM.wig
 bedGraphToBigWig  ${sampleName}.RPM.wig $genomeSizes ${sampleName}.RPM.wig.bw
-rm $bigWigOutputDir/${sampleName}-*.RPM.wig.bw
-ln ${sampleName}.RPM.wig.bw $bigWigOutputDir/${sampleName}-${randSuffix}.RPM.wig.bw
+rm -f $bigWigOutputDir/${sampleName}-*.RPM.wig.bw
+mv ${sampleName}.RPM.wig.bw $bigWigOutputDir/${sampleName}-${randSuffix}.RPM.wig.bw
 echo "track type=bigWig name=\"$sampleName.RPM\" description=\"RPM of $sampleName\" bigDataUrl=$HttpAddress/${sampleName}-${randSuffix}.RPM.wig.bw visibility=Full color=$thisColor alwaysZero=On" > $sampleName.RPM.bwlink.wig
 echo "#number of reads: $numOfReads Unit: RPM=read*1000000/numOfReadsMapped" >> $sampleName.RPM.bwlink.wig
 cp $sampleName.RPM.bwlink.wig $bigWigOutputDir/
@@ -72,8 +72,8 @@ cp $sampleName.RPM.bwlink.wig $bigWigOutputDir/
 #log2(x+1)
 awk -v FS="\t" -v OFS="\t" -v numOfReads=$numOfReads 'BEGIN{logb=log(2)}{if(FNR>1 && $2<$3 ){$4=log($4*1000000.0/numOfReads+1)/logb; print;}}' coverage.wig > ${sampleName}.RPM.l2x1.wig
 bedGraphToBigWig  ${sampleName}.RPM.l2x1.wig $genomeSizes ${sampleName}.RPM.l2x1.wig.bw
-rm $bigWigOutputDir/${sampleName}-*.RPM.l2x1.wig.bw
-ln ${sampleName}.RPM.l2x1.wig.bw $bigWigOutputDir/${sampleName}-${randSuffix}.RPM.l2x1.wig.bw
+rm -f  $bigWigOutputDir/${sampleName}-*.RPM.l2x1.wig.bw
+mv ${sampleName}.RPM.l2x1.wig.bw $bigWigOutputDir/${sampleName}-${randSuffix}.RPM.l2x1.wig.bw
 echo "track type=bigWig name=\"$sampleName.log2(RPM+1)\" description=\"log2(RPM+1) of $sampleName\" bigDataUrl=$HttpAddress/${sampleName}-${randSuffix}.RPM.l2x1.wig.bw visibility=Full color=$thisColor alwaysZero=On" > $sampleName.RPM.l2x1.bwlink.wig
 echo "#number of reads: $numOfReads Unit: log2(RPM+1) RPM=read*1000000/numOfReadsMapped" >> $sampleName.RPM.l2x1.bwlink.wig
 cp $sampleName.RPM.l2x1.bwlink.wig $bigWigOutputDir/
