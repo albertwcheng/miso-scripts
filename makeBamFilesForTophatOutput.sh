b25a75ae772d0305071905059fab9d54042a8d1e
albertwcheng <albertwcheng@gmail.com>
2011-01-29 07:19:24 -0500

    commit

--- makeBamFilesForTophatOutput.sh
+++ makeBamFilesForTophatOutput.sh
@@ -22,10 +22,18 @@ cd $MISOPATH
 
 for sampleDir in $tophatOutputDir/*; do
 
-if [ ! -e $sampleDir/accepted_hits.sam ];  then
-	continue
-fi
+
 
 #echo $sampleDir
-bsub python sam_to_bam.py --convert $sampleDir/accepted_hits.sam $sampleDir/ --ref ${genomeSizes}
+
+if [ -e $sampleDir/accepted_hits.bam ]; then
+	samtools sort $sampleDir/accepted_hits.bam $sampleDir/accepted_hits.sorted
+	samtools index  $sampleDir/accepted_hits.sorted.bam
+else
+	if [ ! -e $sampleDir/accepted_hits.sam ];  then
+		continue
+	fi
+	bsub python sam_to_bam.py --convert $sampleDir/accepted_hits.sam $sampleDir/ --ref ${genomeSizes}
+fi
+
 done
