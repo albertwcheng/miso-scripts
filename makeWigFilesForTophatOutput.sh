c5f0dcc0d15cf901fae2c999f10cc3fcbc0f2163
albertwcheng <albertwcheng@gmail.com>
2011-02-01 00:48:51 -0500

    commit

--- makeWigFilesForTophatOutput.sh
+++ makeWigFilesForTophatOutput.sh
@@ -1,12 +1,12 @@
 if [ $# -ne 2 ]; then
-	echo $0 MISOSettingFile HttpAddress
+	echo $0 MISOSettingFile "HttpAddress[\"\" = don't make bw]"
 	exit
 fi
 
 COLOR[1]="255,0,0"
 COLOR[2]="0,255,0"
 COLOR[3]="0,0,255"
-COLOR[4]="255,255,0"
+COLOR[4]="188,143,143"
 COLOR[5]="255,0,255"
 COLOR[6]="0,255,255"
 COLOR[7]="0,0,0"
@@ -23,9 +23,14 @@ cd ..
 rootDir=`pwd`
 
 tophatOutputDir=$rootDir/tophatOutput
+
+
+
 bigWigOutputDir=$rootDir/bigWigUploads
 
+if [[ $HttpAddress != "" ]]; then
 mkdir $bigWigOutputDir
+fi
 
 sampleI=0
 
@@ -57,28 +62,38 @@ samtools flagstat accepted_hits.sorted.bam > accepted_hits.sorted.bam.flagstat
 
 numOfReads=`awk -v FS=" " '{if(FNR==4 && $2=="mapped"){printf("%s\n",$1);}}' accepted_hits.sorted.bam.flagstat`
 
+if [[ $HttpAddress != "" ]]; then
 tmName=`mktemp`
 randSuffix=`basename $tmName`
-
+fi
 
 awk -v FS="\t" -v OFS="\t" -v numOfReads=$numOfReads '{if(FNR>1 && $2<$3 ){$4=$4*1000000.0/numOfReads; print;}}' coverage.wig > ${sampleName}.RPM.wig
+
+if [[ $HttpAddress != "" ]]; then
 bedGraphToBigWig  ${sampleName}.RPM.wig $genomeSizes ${sampleName}.RPM.wig.bw
 rm -f $bigWigOutputDir/${sampleName}-*.RPM.wig.bw
 mv ${sampleName}.RPM.wig.bw $bigWigOutputDir/${sampleName}-${randSuffix}.RPM.wig.bw
 echo "track type=bigWig name=\"$sampleName.RPM\" description=\"RPM of $sampleName\" bigDataUrl=$HttpAddress/${sampleName}-${randSuffix}.RPM.wig.bw visibility=Full color=$thisColor alwaysZero=On" > $sampleName.RPM.bwlink.wig
 echo "#number of reads: $numOfReads Unit: RPM=read*1000000/numOfReadsMapped" >> $sampleName.RPM.bwlink.wig
 cp $sampleName.RPM.bwlink.wig $bigWigOutputDir/
+fi
+
+gzip ${sampleName}.RPM.wig
+
 
 #log2(x+1)
 awk -v FS="\t" -v OFS="\t" -v numOfReads=$numOfReads 'BEGIN{logb=log(2)}{if(FNR>1 && $2<$3 ){$4=log($4*1000000.0/numOfReads+1)/logb; print;}}' coverage.wig > ${sampleName}.RPM.l2x1.wig
+
+if [[ $HttpAddress != "" ]]; then
 bedGraphToBigWig  ${sampleName}.RPM.l2x1.wig $genomeSizes ${sampleName}.RPM.l2x1.wig.bw
 rm -f  $bigWigOutputDir/${sampleName}-*.RPM.l2x1.wig.bw
 mv ${sampleName}.RPM.l2x1.wig.bw $bigWigOutputDir/${sampleName}-${randSuffix}.RPM.l2x1.wig.bw
 echo "track type=bigWig name=\"$sampleName.log2(RPM+1)\" description=\"log2(RPM+1) of $sampleName\" bigDataUrl=$HttpAddress/${sampleName}-${randSuffix}.RPM.l2x1.wig.bw visibility=Full color=$thisColor alwaysZero=On" > $sampleName.RPM.l2x1.bwlink.wig
 echo "#number of reads: $numOfReads Unit: log2(RPM+1) RPM=read*1000000/numOfReadsMapped" >> $sampleName.RPM.l2x1.bwlink.wig
 cp $sampleName.RPM.l2x1.bwlink.wig $bigWigOutputDir/
+fi
 
-
+gzip ${sampleName}.RPM.l2x1.wig
 
 rm $tmName
 
